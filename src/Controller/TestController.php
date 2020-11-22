9eb898bee29b7291687a0f4e214a4fbe92e84952
unknown <s.slimene19@gmail.com>
2020-11-22 05:10:15 +0100

    Heuraaaa!

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -11,18 +11,17 @@ class TestController {
   public function __construct(LoggerInterface $logger) {
     $this->logger = $logger;
   }
-  /**
-   * @Route("/", name="index")
-   */
-  public function index() {
-    $this->logger->info("Mon Message de log");
-    dd("Ca Fonctionne");
-  }
+  // /**
+  //  * @Route("/", name="index")
+  //  */
+  // public function index() {
+  //   dd("Ca Fonctionne");
+  // }
   /**
    * @Route("/test/{age<\d+>?0}", name="test", methods={"GET", "POST"}, host="127.0.0.1", schemes={"http", "https"})
    */
   public function test(Request $request, $age) {
-
+    $this->logger->info("Mon Message de log");
     return new Response("Vos avez $age ans!");
   }
 }
