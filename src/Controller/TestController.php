acfbbbf4f1ef496d5022c2aa02eb97357ef2982a
unknown <s.slimene19@gmail.com>
2020-11-22 05:44:50 +0100

    finish

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -1,16 +1,13 @@
 <?php
 
 namespace App\Controller;
+use App\Taxes\Calculator;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TestController {
-  protected $logger;
-  public function __construct(LoggerInterface $logger) {
-    $this->logger = $logger;
-  }
   // /**
   //  * @Route("/", name="index")
   //  */
@@ -20,8 +17,10 @@ class TestController {
   /**
    * @Route("/test/{age<\d+>?0}", name="test", methods={"GET", "POST"}, host="127.0.0.1", schemes={"http", "https"})
    */
-  public function test(Request $request, $age) {
-    $this->logger->info("Mon Message de log");
+  public function test(Request $request, $age, LoggerInterface $logger, Calculator $calculator) {
+    $logger->info("Mon Message de log");
+    $tva = $calculator->calcul(100);
+    dump($tva);
     return new Response("Vos avez $age ans!");
   }
 }
